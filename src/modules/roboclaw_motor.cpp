7bb9f80a563997a5fc3e445c0d961c24f45f02d2
Falko Schindler <falko@zauberzeug.com>
2021-11-17 15:39:28 +0100

    speed method for roboclaw motor

--- src/modules/roboclaw_motor.cpp
+++ src/modules/roboclaw_motor.cpp
@@ -4,7 +4,11 @@
 
 RoboClawMotor::RoboClawMotor(std::string name, RoboClaw *roboclaw, unsigned int motor_number) : Module(roboclaw_motor, name)
 {
-    this->motor_number = motor_number;
+    this->motor_number = constrain(motor_number, 1, 2);
+    if (this->motor_number != motor_number)
+    {
+        printf("error: replacing illegal motor number %d with %d\n", motor_number, this->motor_number);
+    }
     this->roboclaw = roboclaw;
 }
 
@@ -19,18 +23,18 @@ void RoboClawMotor::call(std::string method, std::vector<Argument *> arguments)
             return;
         }
         unsigned short int duty = (short int)(constrain(arguments[0]->to_number(), -1, 1) * 32767);
-        if (this->motor_number == 1)
+        this->motor_number == 1 ? this->roboclaw->DutyM1(duty) : this->roboclaw->DutyM2(duty);
+    }
+    else if (method == "speed")
+    {
+        if (arguments.size() != 1 ||
+            !arguments[0]->is_numbery())
         {
-            this->roboclaw->DutyM1(duty);
+            printf("error: expecting 1 number argument for method \"%s.%s\"\n", this->name.c_str(), method.c_str());
+            return;
         }
-        else if (this->motor_number == 2)
-        {
-            this->roboclaw->DutyM2(duty);
-        }
-        else
-        {
-            printf("error: unsupported motor number %d\n", this->motor_number);
-        }
+        unsigned int counts_per_second = arguments[0]->to_number();
+        this->motor_number == 1 ? this->roboclaw->DutyM1(counts_per_second) : this->roboclaw->DutyM2(counts_per_second);
     }
     else
     {
