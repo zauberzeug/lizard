63725ff7e7eba232f9d47bb945216b12057d8996
Falko Schindler <falko@zauberzeug.com>
2021-11-19 15:08:28 +0100

    formatted output of module properties via core module

--- src/modules/core.cpp
+++ src/modules/core.cpp
@@ -1,7 +1,10 @@
 #include "core.h"
 
+#include <stdlib.h>
 #include "esp_system.h"
 #include "esp_timer.h"
+#include "../global.h"
+#include "../utils/strings.h"
 
 Core::Core(std::string name) : Module(core, name)
 {
@@ -27,6 +30,31 @@ void Core::call(std::string method, std::vector<Argument *> arguments)
             return;
         }
         printf("%s\n", arguments[0]->string_value.c_str());
+    }
+    else if (method == "output")
+    {
+        if (arguments.size() != 1 ||
+            arguments[0]->type != string)
+        {
+            printf("error: expecting 1 string argument for method \"%s.%s\"\n", this->name.c_str(), method.c_str());
+            return;
+        }
+        this->output_list.clear();
+        while (!arguments[0]->string_value.empty())
+        {
+            std::string element = cut_first_word(arguments[0]->string_value);
+            std::string module_name = cut_first_word(element, '.');
+            if (!Global::modules.count(module_name))
+            {
+                printf("error: unknown module \"%s\"\n", module_name.c_str());
+                return;
+            }
+            Module *module = Global::modules[module_name];
+            std::string method_name = cut_first_word(element, ':');
+            unsigned int precision = element.empty() ? 0 : atoi(element.c_str());
+            this->output_list.push_back({module, method_name, precision});
+        }
+        this->output = true;
     }
     else
     {
@@ -60,4 +88,17 @@ void Core::set(std::string property_name, double value)
     {
         Module::set(property_name, value);
     }
+}
+
+std::string Core::get_output()
+{
+    static char format_buffer[8];
+    static char output_buffer[1024];
+    int pos = 0;
+    for (auto const &element : this->output_list)
+    {
+        sprintf(format_buffer, "%%.%df", element.precision);
+        pos += sprintf(&output_buffer[pos], format_buffer, element.module->get(element.property_name));
+    }
+    return std::string(output_buffer);
 }
--- src/modules/core.h
+++ src/modules/core.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <list>
+#include <utility>
 #include "module.h"
+
+struct output_element_t
+{
+    Module *module;
+    std::string property_name;
+    unsigned int precision;
+};
 
 class Core : public Module
 {
+private:
+    std::list<struct output_element_t> output_list;
+
 public:
     bool debug = false;
 
@@ -11,4 +23,5 @@ public:
     void call(std::string method, std::vector<Argument *> arguments);
     double get(std::string property_name);
     void set(std::string property_name, double value);
+    std::string get_output();
 };
