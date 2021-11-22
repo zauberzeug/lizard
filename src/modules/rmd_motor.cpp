7bc776b1a13074208f7716ae4f1f19969c0b45c2
Falko Schindler <falko@zauberzeug.com>
2021-11-22 15:51:55 +0100

    pid and acceleration tuning for rmd motor

--- src/modules/rmd_motor.cpp
+++ src/modules/rmd_motor.cpp
@@ -6,16 +6,12 @@ RmdMotor::RmdMotor(std::string name, Can *can) : Module(rmd_motor, name)
 {
     this->can = can;
     this->properties["angle"] = new NumberVariable();
-    this->properties["temperature"] = new IntegerVariable();
-    this->properties["voltage"] = new NumberVariable();
-    this->properties["error"] = new IntegerVariable();
     can->subscribe(this->can_id, this);
 }
 
 void RmdMotor::step()
 {
     this->can->send(this->can_id, 0x92, 0, 0, 0, 0, 0, 0, 0);
-    this->can->send(this->can_id, 0x9a, 0, 0, 0, 0, 0, 0, 0);
 }
 
 void RmdMotor::call(std::string method_name, std::vector<Expression *> arguments)
@@ -104,10 +100,43 @@ void RmdMotor::call(std::string method_name, std::vector<Expression *> arguments
                         *((uint8_t *)(&angle) + 2),
                         *((uint8_t *)(&angle) + 3));
     }
-    else if (method_name == "tune")
+    else if (method_name == "get_health")
     {
-        Module::expect(arguments, 0); // TODO
-        throw std::runtime_error("tune command is not implemented yet");
+        Module::expect(arguments, 0);
+        this->can->send(this->can_id, 0x9a, 0, 0, 0, 0, 0, 0, 0);
+    }
+    else if (method_name == "get_pid")
+    {
+        Module::expect(arguments, 0);
+        this->can->send(this->can_id, 0x30, 0, 0, 0, 0, 0, 0, 0);
+    }
+    else if (method_name == "set_pid")
+    {
+        Module::expect(arguments, 6, integer, integer, integer, integer, integer, integer);
+        this->can->send(this->can_id, 0x32, 0,
+                        arguments[0]->evaluate_integer(),
+                        arguments[1]->evaluate_integer(),
+                        arguments[2]->evaluate_integer(),
+                        arguments[3]->evaluate_integer(),
+                        arguments[4]->evaluate_integer(),
+                        arguments[5]->evaluate_integer());
+    }
+    else if (method_name == "get_acceleration")
+    {
+        Module::expect(arguments, 0);
+        this->can->send(this->can_id, 0x33, 0, 0, 0, 0, 0, 0, 0);
+    }
+    else if (method_name == "set_acceleration")
+    {
+        Module::expect(arguments, 1, numbery);
+        int32_t acceleration = arguments[0]->evaluate_number();
+        this->can->send(this->can_id, 0x34, 0,
+                        0,
+                        0,
+                        *((uint8_t *)(&acceleration) + 0),
+                        *((uint8_t *)(&acceleration) + 1),
+                        *((uint8_t *)(&acceleration) + 2),
+                        *((uint8_t *)(&acceleration) + 3));
     }
     else if (method_name == "clear_errors")
     {
@@ -122,20 +151,37 @@ void RmdMotor::call(std::string method_name, std::vector<Expression *> arguments
 
 void RmdMotor::handle_can_msg(uint32_t id, int count, uint8_t *data)
 {
-    if (data[0] == 0x92)
+    switch (data[0])
+    {
+    case 0x92:
     {
         int64_t value = 0;
         std::memcpy(&value, data + 1, 7);
         this->properties["angle"]->number_value = (value << 8) / 256.0 / 100.0;
+        break;
     }
-    if (data[0] == 0x9a)
+    case 0x9a:
     {
         int8_t temperature = 0;
         std::memcpy(&temperature, data + 1, 1);
-        this->properties["temperature"]->integer_value = temperature;
         uint16_t voltage = 0;
         std::memcpy(&voltage, data + 3, 2);
-        this->properties["voltage"]->number_value = 0.1 * voltage;
-        this->properties["error"]->integer_value = data[7];
+        uint8_t error = data[7];
+        printf("%s health %d %.1f %d\n", this->name.c_str(), temperature, 0.1 * voltage, error);
+        break;
+    }
+    case 0x30:
+    {
+        printf("%s pid %3d %3d %3d %3d %3d %3d\n",
+               this->name.c_str(), data[2], data[3], data[4], data[5], data[6], data[7]);
+        break;
+    }
+    case 0x33:
+    {
+        int32_t acceleration = 0;
+        std::memcpy(&acceleration, data + 4, 4);
+        printf("%s acceleration %d\n", this->name.c_str(), acceleration);
+        break;
+    }
     }
 }
