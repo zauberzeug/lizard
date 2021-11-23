b6e3b2124dcd08896f45c70c09089f4100433768
Falko Schindler <falko@zauberzeug.com>
2021-11-23 15:45:00 +0100

    some cleanup

--- src/modules/roboclaw_motor.cpp
+++ src/modules/roboclaw_motor.cpp
@@ -10,19 +10,19 @@ RoboClawMotor::RoboClawMotor(std::string name, RoboClaw *roboclaw, unsigned int 
         throw std::runtime_error("illegal motor number");
     }
     this->roboclaw = roboclaw;
-    this->properties["encoder"] = new IntegerVariable();
+    this->properties["position"] = new IntegerVariable();
 }
 
 void RoboClawMotor::step()
 {
     uint8_t status;
     bool valid;
-    int32_t encoder = this->motor_number == 1 ? this->roboclaw->ReadEncM1(&status, &valid) : this->roboclaw->ReadEncM2(&status, &valid);
+    int32_t position = this->motor_number == 1 ? this->roboclaw->ReadEncM1(&status, &valid) : this->roboclaw->ReadEncM2(&status, &valid);
     if (!valid)
     {
-        throw std::runtime_error("could not read encoder value");
+        throw std::runtime_error("could not read motor position");
     }
-    this->properties["encoder"]->integer_value = encoder;
+    this->properties["position"]->integer_value = position;
 }
 
 void RoboClawMotor::call(std::string method_name, std::vector<Expression *> arguments)
@@ -31,20 +31,28 @@ void RoboClawMotor::call(std::string method_name, std::vector<Expression *> argu
     {
         Module::expect(arguments, 1, numbery);
         unsigned short int duty = (short int)(constrain(arguments[0]->evaluate_number(), -1, 1) * 32767);
-        this->motor_number == 1 ? this->roboclaw->DutyM1(duty) : this->roboclaw->DutyM2(duty);
+        bool success = this->motor_number == 1 ? this->roboclaw->DutyM1(duty) : this->roboclaw->DutyM2(duty);
+        if (!success)
+        {
+            throw std::runtime_error("could not set duty cycle");
+        }
     }
     else if (method_name == "speed")
     {
         Module::expect(arguments, 1, numbery);
-        unsigned int counts_per_second = arguments[0]->evaluate_number();
-        this->motor_number == 1 ? this->roboclaw->DutyM1(counts_per_second) : this->roboclaw->DutyM2(counts_per_second);
+        unsigned int counts_per_second = constrain(arguments[0]->evaluate_number(), -32767, 32767);
+        bool success = this->motor_number == 1 ? this->roboclaw->DutyM1(counts_per_second) : this->roboclaw->DutyM2(counts_per_second);
+        if (!success)
+        {
+            throw std::runtime_error("could not set speed");
+        }
     }
     else if (method_name == "zero")
     {
         bool success = this->motor_number == 1 ? this->roboclaw->SetEncM1(0) : this->roboclaw->SetEncM2(0);
         if (!success)
         {
-            throw std::runtime_error("could not reset encoder");
+            throw std::runtime_error("could not reset position");
         }
     }
     else
