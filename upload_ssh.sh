6ec516fd18e862a4baf6f5b4e1b4c1fac2cfe972
Falko Schindler <falko@zauberzeug.com>
2021-12-09 10:11:29 +0100

    make sure esptool.py is in path when remotely executing upload command

--- upload_ssh.sh
+++ upload_ssh.sh
@@ -13,4 +13,4 @@ echo "Copying binary files to $host..."
 rsync -zarv --prune-empty-dirs --include="*/" --include="*.bin" --exclude="*" build $host:~/lizard/ || exit 1 # https://stackoverflow.com/a/32527277/3419103
 
 echo "Flashing microcontroller..."
-ssh -t $host 'cd ~/lizard && ./upload_ths1.sh'
+ssh -t $host "bash --login -c 'cd ~/lizard && ./upload_ths1.sh'"
