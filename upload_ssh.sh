a19d9297d39dbd237f291c0e07b25fee52a2f1c0
Falko Schindler <falko@zauberzeug.com>
2021-12-20 15:31:36 +0100

    add option for uploading firmware to expander esp

--- upload_ssh.sh
+++ upload_ssh.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
     echo "Usage:"
-    echo "`basename $0` user@host"
+    echo "`basename $0` [-e/--expander] user@host"
     exit
+fi
+
+if [[ "$1" == "-e" || "$1" == "--expander" ]]
+then
+    shift
+    remote_command="./upload_ths1.sh"
+else
+    remote_command="./upload_expander.sh"
 fi
 
 host=$1
@@ -13,4 +21,4 @@ echo "Copying binary files to $host..."
 rsync -zarv --prune-empty-dirs --include="*/" --include="*.bin" --exclude="*" build $host:~/lizard/ || exit 1 # https://stackoverflow.com/a/32527277/3419103
 
 echo "Flashing microcontroller..."
-ssh -t $host "bash --login -c 'cd ~/lizard && ./upload_ths1.sh'"
+ssh -t $host "bash --login -c 'cd ~/lizard && $remote_command'"
