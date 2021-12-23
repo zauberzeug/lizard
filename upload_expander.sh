f17f0b28128ecc773ae5b0ebcfda690616b18128
Falko Schindler <falko@zauberzeug.com>
2021-12-23 14:28:17 +0100

    use new command for deactivating uart1

--- upload_expander.sh
+++ upload_expander.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Configuring pins..."
-echo 'g13 = Output(13)' > /dev/ttyTHS1
-echo 'g14 = Output(14)' > /dev/ttyTHS1
-
 echo "Bringing microcontroller into flash mode..."
-echo 'g13.off()' > /dev/ttyTHS1
-echo 'g14.off()' > /dev/ttyTHS1
-echo 'g14.on()' > /dev/ttyTHS1
-echo 'g13.on()' > /dev/ttyTHS1
+echo '!~' > /dev/ttyTHS1
 
 echo "Flashing microcontroller..."
 dev=/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_f276e472a1e3ea118d06177c994a5d01-if00-port0
@@ -28,5 +21,4 @@ dev=/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_f27
     0x10000 build/lizard.bin
 
 echo "Bringing microcontroller back into normal operation mode..."
-echo 'g14.off()' > /dev/ttyTHS1
-echo 'g14.on()' > /dev/ttyTHS1
+echo 'core.restart()' > /dev/ttyTHS1
