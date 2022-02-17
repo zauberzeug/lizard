917629d09edc993302d6f09003864f1c318c01be
Falko Schindler <falko@zauberzeug.com>
2022-02-17 18:03:19 +0100

    add expander.run() method

--- main/modules/expander.cpp
+++ main/modules/expander.cpp
@@ -49,9 +49,15 @@ void Expander::step() {
 }
 
 void Expander::call(const std::string method_name, const std::vector<ConstExpression_ptr> arguments) {
-    static char buffer[1024];
-    int pos = std::sprintf(buffer, "core.%s(", method_name.c_str());
-    pos += write_arguments_to_buffer(arguments, &buffer[pos]);
-    pos += std::sprintf(&buffer[pos], ")\n");
-    this->serial->write_chars(buffer, pos);
+    if (method_name == "run") {
+        Module::expect(arguments, 1, string);
+        std::string command = arguments[0]->evaluate_string() + '\n';
+        this->serial->write_chars(command.c_str(), command.length());
+    } else {
+        static char buffer[1024];
+        int pos = std::sprintf(buffer, "core.%s(", method_name.c_str());
+        pos += write_arguments_to_buffer(arguments, &buffer[pos]);
+        pos += std::sprintf(&buffer[pos], ")\n");
+        this->serial->write_chars(buffer, pos);
+    }
 }
