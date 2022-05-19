3a422efe8259bcf85e008295af8aaa8cebc68a38
Amaryllis Kulla <amaryllis@zauberzeug.com>
2022-05-19 15:51:06 +0200

    bluetooth: call Core::keep_alive() on each received message

--- main/modules/bluetooth.cpp
+++ main/modules/bluetooth.cpp
@@ -1,5 +1,9 @@
 #include "bluetooth.h"
+#include "core.h"
 #include "uart.h"
+
+// somewhat of a hack, maybe call ->keep_alive in parse_lizard?
+extern Core_ptr core_module;
 
 Bluetooth::Bluetooth(const std::string name, const std::string device_name, void (*message_handler)(const char *))
     : Module(bluetooth, name), device_name(device_name) {
@@ -7,6 +11,7 @@ Bluetooth::Bluetooth(const std::string name, const std::string device_name, void
         try {
             std::string message_string(message.data(), message.length());
             message_handler(message_string.c_str());
+            core_module->keep_alive();
         } catch (const std::exception &e) {
             echo("error in bluetooth message handler: %s", e.what());
         }
