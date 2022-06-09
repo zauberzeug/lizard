de46bad47ba78e62b58b99f7381b69e9c56fa632
Amaryllis Kulla <amaryllis@zauberzeug.com>
2022-06-09 16:43:20 +0200

    roboclaw_wheels: handle encoder wrap-around without skipping values

--- main/modules/roboclaw_wheels.cpp
+++ main/modules/roboclaw_wheels.cpp
@@ -11,23 +11,35 @@ RoboClawWheels::RoboClawWheels(const std::string name, const RoboClawMotor_ptr l
     this->properties["m_per_tick"] = std::make_shared<NumberVariable>(1);
 }
 
+/* Catch unsigned wrap-around by detecting large jumps in encoder deltas */
+static double difference_wrapped_u32(double current_value, double last_value) {
+    double diff = current_value - last_value;
+
+    if (diff > (double(UINT32_MAX / 2))) {
+        /* Underflow */
+        diff = diff - double(UINT32_MAX) - 1;
+    } else if (diff < -double(UINT32_MAX / 2)) {
+        /* Overflow */
+        diff = double(UINT32_MAX) + diff + 1;
+    }
+
+    return diff;
+}
+
 void RoboClawWheels::step() {
     double left_position = this->left_motor->get_position();
     double right_position = this->right_motor->get_position();
 
     if (initialized) {
-        double d_left_position = left_position - last_left_position;
-        double d_right_position = right_position - last_right_position;
+        double d_left_position = difference_wrapped_u32(left_position, last_left_position);
+        double d_right_position = difference_wrapped_u32(right_position, last_right_position);
 
-        /* Catch unsigned wrap-around by detecting large jumps in encoder deltas */
-        if (std::abs(d_left_position) < (UINT32_MAX / 2) && std::abs(d_right_position) < (UINT32_MAX / 2)) {
-            unsigned long int d_micros = micros_since(last_micros);
-            const double scale = this->properties.at("m_per_tick")->number_value;
-            double left_speed = (d_left_position * scale) / d_micros * 1000000;
-            double right_speed = (d_right_position * scale) / d_micros * 1000000;
-            this->properties.at("linear_speed")->number_value = (left_speed + right_speed) / 2;
-            this->properties.at("angular_speed")->number_value = (right_speed - left_speed) / this->properties.at("width")->number_value;
-        }
+        unsigned long int d_micros = micros_since(last_micros);
+        const double scale = this->properties.at("m_per_tick")->number_value;
+        double left_speed = (d_left_position * scale) / d_micros * 1000000;
+        double right_speed = (d_right_position * scale) / d_micros * 1000000;
+        this->properties.at("linear_speed")->number_value = (left_speed + right_speed) / 2;
+        this->properties.at("angular_speed")->number_value = (right_speed - left_speed) / this->properties.at("width")->number_value;
     }
 
     last_micros = micros();
