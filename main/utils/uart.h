abb41e4eef2c0e85e82583989cfab9237c69fece
SeaTechRC <153106523+SeaTechRC@users.noreply.github.com>
2024-06-26 11:26:36 +0200

    Renamed defition of make_check_line

--- main/utils/uart.h
+++ main/utils/uart.h
@@ -2,5 +2,5 @@
 
 void echo(const char *fmt, ...);
 int strip(char *buffer, int len);
-int make_check_line(char *buffer, int len);
+int make_checked_line(char *buffer, int len);
 int check(char *buffer, int len);
