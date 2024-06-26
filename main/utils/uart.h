3b96c22c21a780e95652062dfb49804be4d71565
SeaTechRC <153106523+SeaTechRC@users.noreply.github.com>
2024-06-26 10:57:43 +0200

    Replaced append_checksum to make_check_line

--- main/utils/uart.h
+++ main/utils/uart.h
@@ -2,5 +2,5 @@
 
 void echo(const char *fmt, ...);
 int strip(char *buffer, int len);
-int append_checksum(char *buffer, int len, int maxlen);
+int make_check_line(char *buffer, int len);
 int check(char *buffer, int len);
