dd4bf4d80862166e532740015d1d3d114c8bb59c
Jens Ogorek <Jens.ogorek@gmail.com>
2024-08-14 15:46:07 +0200

    return to normal

--- main/modules/expander.cpp
+++ main/modules/expander.cpp
@@ -88,12 +88,6 @@ void Expander::call(const std::string method_name, const std::vector<ConstExpres
         if (!success) {
             throw std::runtime_error("could not flash expander \"" + this->name + "\"");
         }
-    } else if (method_name == "xoff") {
-        Module::expect(arguments, 0);
-        this->serial->write_checked_line("\x13", 1);
-    } else if (method_name == "xon") {
-        Module::expect(arguments, 0);
-        this->serial->write_checked_line("\x11", 1);
     } else {
         static char buffer[1024];
         int pos = std::sprintf(buffer, "core.%s(", method_name.c_str());
