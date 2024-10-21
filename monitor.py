3fc940be620d94967acb70463e0cc7e20dcf618a
Johannes-Thiel <jojo.thadfun@gmail.com>
2024-10-21 15:10:38 +0200

    revert asyncio event_loop

--- monitor.py
+++ monitor.py
@@ -97,8 +97,7 @@ def serial_connection() -> serial.Serial:
 
 if __name__ == '__main__':
     with serial_connection() as port:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        loop = asyncio.get_event_loop_policy().get_event_loop()
         loop.create_task(send())
         loop.run_in_executor(None, receive)
         loop.run_forever()
