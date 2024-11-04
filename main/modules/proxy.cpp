f5403ec0da1df9fcfa6a3ea8d487288f4495aee3
Jens Ogorek <Jens.ogorek@gmail.com>
2024-11-04 16:24:04 +0100

    update: improved "is_ready" handling with step and expander module's ready signal

--- main/modules/proxy.cpp
+++ main/modules/proxy.cpp
@@ -13,6 +13,13 @@ Proxy::Proxy(const std::string name,
     expander->add_proxy(name, module_type, arguments);
 }
 
+void Proxy::step() {
+    // Update proxy's ready state based on expander's state
+    this->properties.at("is_ready")->boolean_value =
+        expander->get_property("is_ready")->boolean_value;
+    Module::step();
+}
+
 void Proxy::call(const std::string method_name, const std::vector<ConstExpression_ptr> arguments) {
     static char buffer[256];
     int pos = csprintf(buffer, sizeof(buffer), "%s.%s(", this->name.c_str(), method_name.c_str());
@@ -24,7 +31,6 @@ void Proxy::call(const std::string method_name, const std::vector<ConstExpressio
 void Proxy::write_property(const std::string property_name, const ConstExpression_ptr expression, const bool from_expander) {
     if (!this->properties.count(property_name)) {
         this->properties[property_name] = std::make_shared<Variable>(expression->type);
-        this->properties.at("is_ready")->boolean_value = true;
     }
     if (!from_expander) {
         static char buffer[256];
