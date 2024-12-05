0877ba7f4bdee626fca0feee74f6e5605a1a8671
Falko Schindler <falko@zauberzeug.com>
2024-12-05 14:36:04 +0100

    reduce diff (again)

--- main/parser.h
+++ main/parser.h
@@ -2000,7 +2000,7 @@ static bool OWL_DONT_INLINE owl_default_tokenizer_advance(struct owl_default_tok
                 size_t j = 0;
                 for (size_t i = 0;
                 i < content_length;
-                i++) {
+                ++i) {
                     if (string[i] == '\\' && i + 1 < content_length) {
                         i++;
                         output[j++] = ESCAPE_CHAR(string[i], tokenizer->info);
