4645ad6a6862acadf9d4c4a5fce53aa4e87a0795
Jens Ogorek <Jens.ogorek@gmail.com>
2024-12-12 16:01:17 +0100

    let proxy error if expander not ready

--- main/modules/proxy.cpp
+++ main/modules/proxy.cpp
@@ -3,7 +3,7 @@
 #include "../utils/uart.h"
 #include "driver/uart.h"
 #include <memory>
-
+#include <stdexcept>
 Proxy::Proxy(const std::string name,
              const std::string expander_name,
              const std::string module_type,
@@ -12,6 +12,9 @@ Proxy::Proxy(const std::string name,
     : Module(proxy, name), expander(expander) {
     this->properties = Module::get_module_defaults(module_type);
     this->properties["is_ready"] = expander->get_property("is_ready");
+    if (this->properties["is_ready"]->boolean_value == false) {
+        throw std::runtime_error("Proxy: Expander \"" + expander->name + "\" is not ready");
+    }
     expander->add_proxy(name, module_type, arguments);
 }
 
