9d3c57934f55228323e8fa560b18381141358e44
Jens Ogorek <Jens.ogorek@gmail.com>
2024-12-13 13:58:20 +0100

    [update] handling changed to not crash the startup function

--- main/modules/proxy.cpp
+++ main/modules/proxy.cpp
@@ -11,15 +11,18 @@ Proxy::Proxy(const std::string name,
              const std::vector<ConstExpression_ptr> arguments)
     : Module(proxy, name), expander(expander) {
     this->properties = Module::get_module_defaults(module_type);
-    this->properties["is_ready"] = expander->get_property("is_ready");
-    if (this->properties["is_ready"]->boolean_value == false) {
-        throw std::runtime_error("Proxy: Expander \"" + expander->name + "\" is not ready");
+    this->properties["is_ready"] = std::make_shared<BooleanVariable>(false);
+
+    if (this->expander->get_property("is_ready")->boolean_value) {
+        this->expander->send_proxy(name, module_type, arguments);
+        this->properties["is_ready"] = std::make_shared<BooleanVariable>(true);
+    } else {
+        // TODO: error code handling
     }
-    this->expander->write_proxy(name, module_type, arguments);
 }
 
 void Proxy::call(const std::string method_name, const std::vector<ConstExpression_ptr> arguments) {
-    this->expander->write_call(method_name, arguments);
+    this->expander->send_call(method_name, arguments);
 }
 
 void Proxy::write_property(const std::string property_name, const ConstExpression_ptr expression, const bool from_expander) {
@@ -28,7 +31,7 @@ void Proxy::write_property(const std::string property_name, const ConstExpressio
         echo("%s: Unknown property \"%s\"", this->name.c_str(), property_name.c_str());
     }
     if (!from_expander) {
-        this->expander->write_property(this->name, property_name, expression);
+        this->expander->send_property(this->name, property_name, expression);
     }
     Module::get_property(property_name)->assign(expression);
 }
