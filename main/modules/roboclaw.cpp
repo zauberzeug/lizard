3ee9d48d0e3dc75a78487d7474b50a73a77dd983
Jens Ogorek <Jens.ogorek@gmail.com>
2025-01-13 17:27:39 +0100

    [update] roboclaw default calling

--- main/modules/roboclaw.cpp
+++ main/modules/roboclaw.cpp
@@ -13,7 +13,8 @@ const std::map<std::string, Variable_ptr> RoboClaw::get_defaults() {
 
 RoboClaw::RoboClaw(const std::string name, const ConstSerial_ptr serial, const uint8_t address)
     : Module(roboclaw, name), address(address), serial(serial) {
-    this->properties = RoboClaw::get_defaults();
+    auto defaults = RoboClaw::get_defaults();
+    this->properties.insert(defaults.begin(), defaults.end());
 }
 
 void RoboClaw::step() {
