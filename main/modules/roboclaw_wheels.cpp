c046b4ad4e6fd31e11570cfdec408e23cb0e7e3c
Jens Ogorek <Jens.ogorek@gmail.com>
2025-01-13 17:27:27 +0100

    [update] roboclaw wheels default calling

--- main/modules/roboclaw_wheels.cpp
+++ main/modules/roboclaw_wheels.cpp
@@ -14,7 +14,8 @@ const std::map<std::string, Variable_ptr> RoboClawWheels::get_defaults() {
 
 RoboClawWheels::RoboClawWheels(const std::string name, const RoboClawMotor_ptr left_motor, const RoboClawMotor_ptr right_motor)
     : Module(roboclaw_wheels, name), left_motor(left_motor), right_motor(right_motor) {
-    this->properties = RoboClawWheels::get_defaults();
+    auto defaults = RoboClawWheels::get_defaults();
+    this->properties.insert(defaults.begin(), defaults.end());
 }
 
 /* Catch unsigned wrap-around by detecting large jumps in encoder deltas */
