f12ef77706bec4e512be525ad691b4bb78da69c5
Falko Schindler <falko@zauberzeug.com>
2025-05-09 14:08:59 +0200

    refactor and cleanup esp.py and flash.py

--- flash.py
+++ flash.py
@@ -6,7 +6,7 @@ from esp import Esp
 
 
 def show_help() -> None:
-    print(f'{sys.argv[0]} [nano | xavier | orin] [nand | v05] [usb | /dev/<name>] [enable] [-e | --erase] [reset]')
+    print(f'{sys.argv[0]} [nano | xavier | orin] [nand | v05] [usb | /dev/<name>] [enable] [disable] [-e | --erase] [reset]')
     print('   -e, --erase   erase the flash before flashing the new firmware')
     print('   nano          flashing Jetson Nano (default)')
     print('   xavier        flashing Jetson Xavier')
@@ -16,6 +16,7 @@ def show_help() -> None:
     print('   usb           use /dev/tty.SLAB_USBtoUART as serial device')
     print('   /dev/<name>   use /dev/<name> as serial device')
     print('   enable        enable the ESP32 microcontroller')
+    print('   disable       disable the ESP32 microcontroller')
     print('   reset         reset the ESP32 microcontroller')
 
 
@@ -24,15 +25,17 @@ if any(h in sys.argv for h in ['--help', '-help', 'help', '-h']):
     sys.exit()
 
 erase_flash = any(e in sys.argv for e in ['-e', '--erase'])
-device = None
-if 'usb' in sys.argv:
-    device = '/dev/tty.SLAB_USBtoUART'
+device = '/dev/tty.SLAB_USBtoUART' if 'usb' in sys.argv else None
 for p in sys.argv:
     if p.startswith('/dev/'):
         device = p
 
-esp = Esp(nand='nand' in sys.argv, xavier='xavier' in sys.argv,
-          orin='orin' in sys.argv, v05='v05' in sys.argv, device=device)
+esp = Esp(
+    jetson='xavier' if 'xavier' in sys.argv else 'orin' if 'orin' in sys.argv else 'nano',
+    nand='nand' in sys.argv,
+    v05='v05' in sys.argv,
+    device=device,
+)
 
 
 # Check if the device should be enabled
