18f84bc01f9f0d6bcb95df39e75323a5cd694606
Falko Schindler <falko@zauberzeug.com>
2025-05-14 11:45:38 +0200

    allow running without --jetson

--- espresso.py
+++ espresso.py
@@ -11,7 +11,7 @@ from contextlib import contextmanager
 parser = argparse.ArgumentParser(description='Flash and control an ESP32 microcontroller from a Jetson board')
 
 parser.add_argument('command', choices=['flash', 'enable', 'disable', 'reset', 'erase'], help='Command to execute')
-parser.add_argument('-j', '--jetson', choices=['nano', 'xavier', 'orin'], help='Jetson board type')
+parser.add_argument('-j', '--jetson', choices=['nano', 'xavier', 'orin'], default=None, help='Jetson board type')
 parser.add_argument('--nand', action='store_true', help='Board has NAND gates')
 parser.add_argument('--swap_pins', action='store_true',
                     help='Swap EN and G0 pins (for Jetson Orin with piggyback older than V0.5)')
@@ -20,22 +20,22 @@ parser.add_argument('--partition-table', default='build/partition_table/partitio
                     help='Path to partition table')
 parser.add_argument('--firmware', default='build/lizard.bin', help='Path to firmware binary')
 parser.add_argument('-d', '--dry-run', action='store_true', help='Dry run')
-parser.add_argument('device', default='/dev/tty.SLAB_USBtoUART', help='Serial device path')
+parser.add_argument('device', default='/dev/tty.SLAB_USBtoUART', help='Serial device path (overwritten by --jetson)')
 
 args = parser.parse_args()
 
-EN_PIN = {'nano': 216, 'xavier': 436, 'orin': 460}[args.jetson]
-G0_PIN = {'nano': 50, 'xavier': 428, 'orin': 492}[args.jetson]
+EN_PIN = {'nano': 216, 'xavier': 436, 'orin': 460}.get(args.jetson, -1)
+G0_PIN = {'nano': 50, 'xavier': 428, 'orin': 492}.get(args.jetson, -1)
 GPIO_EN = 'PR.04' if args.jetson == 'orin' else f'gpio{EN_PIN}'
 GPIO_G0 = 'PAC.06' if args.jetson == 'orin' else f'gpio{G0_PIN}'
 if args.swap_pins:
     EN_PIN, G0_PIN = G0_PIN, EN_PIN
     GPIO_EN, GPIO_G0 = GPIO_G0, GPIO_EN
-DEVICE = args.device or {
+DEVICE = {
     'nano': '/dev/ttyTHS1',
     'xavier': '/dev/ttyTHS0',
     'orin': '/dev/ttyTHS0',
-}[args.jetson]
+}.get(args.jetson, args.device)
 ON = 1 if args.nand else 0
 OFF = 0 if args.nand else 1
 DRY_RUN = args.dry_run
@@ -44,6 +44,9 @@ DRY_RUN = args.dry_run
 @contextmanager
 def pin_config() -> Generator[None, None, None]:
     """Configure the EN and G0 pins to control the microcontroller."""
+    if not args.jetson:
+        yield
+        return
     print_bold('Configuring EN and G0 pins...')
     write_gpio('export', EN_PIN)
     time.sleep(0.5)
@@ -62,6 +65,9 @@ def pin_config() -> Generator[None, None, None]:
 @contextmanager
 def flash_mode() -> Generator[None, None, None]:
     """Bring the microcontroller into flash mode."""
+    if not args.jetson:
+        yield
+        return
     print_bold('Bringing the microcontroller into flash mode...')
     write_gpio(f'{GPIO_EN}/value', ON)
     time.sleep(0.5)
